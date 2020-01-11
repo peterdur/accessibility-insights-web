6f1062bea3409fd8e012b03be24e8a11c43d4211
Peter Durham <peterdur@microsoft.com>
2020-01-10 16:49:28 -0800

    Tweak building Edge manifest

--- new-appx.ps1
+++ new-appx.ps1
@@ -3,6 +3,7 @@
 
 if (Test-Path ./drop/productionEdge) { Remove-Item ./drop/productionEdge -Recurse }
 if (Test-Path ./drop/productionEdge.appx) { Remove-Item ./drop/productionEdge.appx }
-Copy-Item ./drop/extension/production ./drop/productionEdge -Recurse
+New-Item -Path ./drop/productionEdge/ -ItemType Directory
+Copy-Item ./drop/extension/production/product ./drop/productionEdge/Extension -Recurse
 Copy-Item ./AppXManifest.xml ./drop/productionEdge/AppXManifest.xml
 &"c:\Program Files (x86)\Windows Kits\10\bin\10.0.17134.0\x64\makeappx.exe" pack /h SHA256 /d .\drop\productionEdge /p .\drop\productionEdge.appx
